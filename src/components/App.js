7ee3ed7007a821899c5384fe6c6ee0cc016178ef
sparveen7618 <104000560+sparveen7618@users.noreply.github.com>
2022-06-17 10:56:39 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main"><p>I am learning React. My life is getting better.</p></div>
+  )
+}
+
+
+export default App;
